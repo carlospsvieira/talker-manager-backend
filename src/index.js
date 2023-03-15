47c2ced3272c5708e8dc649c120cde201994be8b
Carlos Vieira <carlepsvieira@gmail.com>
2023-03-15 11:33:23 -0300

    req 2 returning talker by id and hanling error

--- src/index.js
+++ src/index.js
@@ -7,15 +7,30 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
+
 // req 01 endpoint returns status 200 with array of all talkers //
 app.get('/talker', async (req, res) => {
   const talkers = await getTalkers();
   res.status(200).json(talkers);
 });
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+// req 02 get talker by id //
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await getTalkers();
+  const wantedTalker = talkers.find((talker) => talker.id === Number(id));
+
+  if (!wantedTalker) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+
+  return res.status(200).json(wantedTalker);
 });
 
 // listen to port 3001 //
